56e0fdf2a3227623c9255a109024e6031c054048
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-01-01 18:30:56 +0800

    style: add header

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/01 18:30:39 by jatan             #+#    #+#             */
+/*   Updated: 2023/01/01 18:30:39 by jatan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 #include "minirt.h"
 
@@ -11,6 +23,7 @@ int	main(void)
 	mlx.addr = mlx_get_data_addr(mlx.img, &mlx.bits_per_pixel, &mlx.line_length,
 			&mlx.endian);
 	render_gradient(&mlx);
+
 	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
 	mlx_hook(mlx.win, 3, 1L << 1, handle_key_release, &mlx);
 	mlx_loop(mlx.mlx);
