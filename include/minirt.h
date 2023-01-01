6d3e010f236723425287412ec048215b35eef531
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-01-01 22:06:44 +0800

    chore: move code to utils

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 12:32:10 by jatan             #+#    #+#             */
-/*   Updated: 2022/12/30 22:40:48 by jatan            ###   ########.fr       */
+/*   Updated: 2023/01/01 22:04:00 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,15 @@
 
 # include "objects.h"
 
-void		put_pixel(t_data *data, int x, int y, int color);
-int			handle_key_release(int keycode, t_data *vars);
 
 void		render_gradient(t_data *data);
+void		render_image(t_data *data);
+
+
+int			create_trgb(int t, int r, int g, int b);
+int			create_trgb_vec(t_vector *color);
+void		put_pixel(t_data *data, int x, int y, int color);
+int			handle_key_release(int keycode, t_data *vars);
 
 double		get(t_vector *vec, enum e_vec_type c);
 void		set(t_vector *vec, enum e_vec_type c, double value);
