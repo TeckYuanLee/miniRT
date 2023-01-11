43e735e7ca18b4e1e00abb5bddc393f858798c5e
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-01-11 18:04:10 +0800

    style: comment on main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 18:30:39 by jatan             #+#    #+#             */
-/*   Updated: 2023/01/09 14:45:25 by jatan            ###   ########.fr       */
+/*   Updated: 2023/01/11 18:03:49 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,21 @@ void	init_data(t_data *data)
 			&data->img.endian);
 }
 
+
+/**
+ * @brief a function to call the ray tracing function and put image on window
+ * Can just change the `render_image` function to your own ray trace function
+ *
+ * @param data
+ * @return int
+ */
 int	render(t_data *data)
 {
 	if (data->win == NULL)
 		return (1);
 	render_image(data, &data->scene, data->objects);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.mlx_img, 0, 0);
+	ft_putstr_fd("Rendered image and putted image to window...\n", 1);
 	return (0);
 }
 
@@ -57,7 +66,7 @@ int	main(int argc, char **argv)
 	free(configs);
 	init_data(&data);
 
-	mlx_loop_hook(data.mlx, &render, &data);
+	render(&data);
 	mlx_hook(data.win, 3, 1L << 1, handle_key_release, &data);
 
 	mlx_loop(data.mlx);
