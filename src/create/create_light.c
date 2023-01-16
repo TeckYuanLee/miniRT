880d043c244cf0e6ff3fe726e51242f073426dd4
Jachokoreto <49505493+Jachokoreto@users.noreply.github.com>
2023-01-16 14:56:10 +0800

    style: clean create light function

--- src/create/create_light.c
+++ src/create/create_light.c
@@ -6,7 +6,7 @@
 /*   By: jatan <jatan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/07 13:10:07 by jatan             #+#    #+#             */
-/*   Updated: 2023/01/16 13:35:23 by jatan            ###   ########.fr       */
+/*   Updated: 2023/01/16 14:55:40 by jatan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,13 @@ int	create_light(t_scene *scene, t_list **objects, char **conf)
 		perror(RED"Error: create light: "RESET);
 		return (-1);
 	}
-	// light = &(*scene->light);
-	// if (light->init != 0)
-	// 	return (error("Have multiple Light config", -1));
 	if (is_vec_format(conf[1]) && is_vec_format(conf[3]))
 		return (error("Light vector in wrong format", -1));
 	light->coor = convrt_to_vec(conf[1]);
 	light->ratio = ft_atod(conf[2]);
 	if (light->ratio < 0.0 || light->ratio > 1.0)
 		return (error("Light ratio not in range [0.0, 1.0]", -1));
-	// light->color = v_div_d(convrt_to_vec(conf[3]), 255);
 	light->color = convrt_to_int(conf[3]);
-	ft_lstadd_front(&scene->light, ft_lstnew(light));
-	// light->init = 1;
+	ft_lstadd_front(&scene->lights, ft_lstnew(light));
 	return (0);
 }
